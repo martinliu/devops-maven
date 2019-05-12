42cace506274b43cf58f22daf794968548d04172
Martin Liu <liuzh66@gmail.com>
2019-05-12 08:17:33 -0400

    add 5 errors

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+
+  // TODO: Add javadoc comment
+  public String greet( String someone) {
+   return String.format("Hello, %s!", someone);
+   }
+
 }
