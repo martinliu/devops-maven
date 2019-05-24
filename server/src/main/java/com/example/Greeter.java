d7dd3ef8514b01fbbe926c31c981323713a3b8e0
Martin Liu <liuzh66@gmail.com>
2019-05-25 00:50:00 +0800

    restore.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,8 @@ public class Greeter {
 
   }
 
-
   // TODO: Add javadoc comment
-  public String greet( String someone) {
-   return String.format("Hello, %s!", someone);
-   }
-
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
 }
