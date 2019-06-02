0826d3b9ee27000f3135d6f4c585f1a37fa0fb1a
Martin Liu <liuzh66@gmail.com>
2019-06-02 14:36:08 +0800

    test .

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
 
-  final public String greet(String final someone) {
+  final public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
